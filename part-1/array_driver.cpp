c139190343ad90dfa1bbc696c2e1e2b58c7b73d1
P3t3r03 <99038634+P3t3r03@users.noreply.github.com>
2024-02-14 17:03:50 +0000

    test

--- part-1/array_driver.cpp
+++ part-1/array_driver.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     print(A, "Array before inserting any elment = ");
     for (size_t i = 0; i < 5; ++i) {
         array_insert(A, 0, static_cast<float>(i));
-        print(A, "Array after inserting " + std::to_string(i) +
+        print(A, "Array after inserting stuff" + std::to_string(i) +
                      " at position 0 = ");
     }
     return 0;
