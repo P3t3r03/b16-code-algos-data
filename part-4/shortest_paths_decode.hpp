a64f3253267826d072440095095976cd3f2f1717
P3t3r03 <99038634+P3t3r03@users.noreply.github.com>
2024-02-27 11:32:19 +0000

    Finished the mandatory parts of problem sheet

--- part-4/shortest_paths_decode.hpp
+++ part-4/shortest_paths_decode.hpp
@@ -4,10 +4,21 @@
 #include "graph.hpp"
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
 inline std::vector<int> decode(const std::vector<hop_t> &DP, int v)
 {
-    // WRITE YOUR CODE HERE
+    std::vector<int> path;
+    while(v != -1) {
+        path.push_back(v);
+        v = DP[v].vertex;
+    }
+
+    std::reverse(path.begin(), path.end());
+    if(path.size() < 2){
+        return {};
+    }
+    return path;
 }
 
 #endif // __shortest_paths_decode__
--- part-4/shortest_paths_sparse.cpp
+++ part-4/shortest_paths_sparse.cpp
@@ -2,6 +2,7 @@
 #include <priority_queue.hpp>
 
 #include <cmath>
+#include <iostream>
 
 std::vector<hop_t> bellman_ford(const SparseGraph &graph, const int source,
                                 bool &has_negative_cycle)
@@ -9,20 +10,21 @@ std::vector<hop_t> bellman_ford(const SparseGraph &graph, const int source,
     const int V = static_cast<int>(graph.size());
     auto DP = std::vector<hop_t>(V, {inf, -1});
     DP[source].weight = 0.0;
+    has_negative_cycle = false;
 
     for(int i = 0; i < V-1 ; i++) {
         has_negative_cycle = false;
-        for(int r = 0; r < V; r++) {
-            for(int v = 0; v< V; v++) {
-                
+        for(int u = 0; u < V; u++) {
+            for(const auto& edge : graph[u]) {
+                int v = edge.vertex;
+                float weight = edge.weight;
+                if (DP[u].weight < inf && DP[u].weight + weight < DP[v].weight) {
+                    DP[v].weight = DP[u].weight + weight;
+                    DP[v].vertex = u;
+                }
             }
         }
-        
     }
-
-
-
-
     return DP;
 }
 
@@ -37,9 +39,36 @@ std::vector<hop_t> dijkstra(const SparseGraph &graph, const int source)
     assert(source >= 0);
     assert(source < (signed)graph.size());
 
+    const int V = static_cast<int>(graph.size());
     auto DP = std::vector<hop_t>(graph.size(), {inf, -1});
+    DP[source].weight = 0.0;
 
-    // WRITE YOUR CODE HERE
+    std::vector<bool> Open(V, true);
+
+    while(true) {
+        float D_star = inf;
+        int v_star = -1;
+        for(int v = 0; v < V; v++) {
+            if(Open[v] && DP[v].weight < D_star) {
+                D_star = DP[v].weight;
+                v_star = v;
+            }
+        }
+        if(v_star == -1) {
+            break;
+        }
+
+        Open[v_star] = false;
+
+        for(const auto& near : graph[v_star]) {
+            int v = near.vertex;
+            float weight = near.weight;
+            if(Open[v] && DP[v_star].weight + weight < DP[v].weight) {
+                DP[v].weight = DP[v_star].weight + weight;
+                DP[v].vertex = v_star;
+            }
+        }
+    }
 
     return DP;
 }
